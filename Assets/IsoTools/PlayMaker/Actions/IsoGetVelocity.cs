6c7a10c3480ea23966ba5bc3ed3dd9edf7507f8a
BlackMATov <blackmatov@gmail.com>
2015-12-20 20:05:27 +0600

    New PlayMaker actions: IsoGetVelocity and IsoSetVelocity

---
+++ Assets/IsoTools/PlayMaker/Actions/IsoGetVelocity.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using HutongGames.PlayMaker;
+using IsoTools.PlayMaker.Internal;
+
+namespace IsoTools.PlayMaker.Actions {
+	[ActionCategory("IsoTools")]
+	[HutongGames.PlayMaker.Tooltip(
+		"Gets the Velocity of a IsoRigidbody and stores it " +
+		"in a Vector3 Variable or each Axis in a Float Variable")]
+	public class IsoGetVelocity : IsoComponentAction<IsoRigidbody> {
+		[RequiredField]
+		[CheckForComponent(typeof(IsoRigidbody))]
+		public FsmOwnerDefault gameObject;
+
+		[UIHint(UIHint.Variable)]
+		public FsmVector3 storeVector;
+
+		[UIHint(UIHint.Variable)]
+		public FsmFloat storeX;
+
+		[UIHint(UIHint.Variable)]
+		public FsmFloat storeY;
+
+		[UIHint(UIHint.Variable)]
+		public FsmFloat storeZ;
+
+		public bool everyFrame;
+
+		public override void Reset() {
+			gameObject  = null;
+			storeVector = null;
+			storeX      = null;
+			storeY      = null;
+			storeZ      = null;
+			everyFrame  = false;
+		}
+
+		public override void OnEnter() {
+			DoAction();
+			if ( !everyFrame ) {
+				Finish();
+			}
+		}
+
+		public override void OnUpdate() {
+			DoAction();
+		}
+
+		void DoAction() {
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if ( UpdateCache(go) ) {
+				var value         = isoRigidbody.velocity;
+				storeVector.Value = value;
+				storeX.Value      = value.x;
+				storeY.Value      = value.y;
+				storeZ.Value      = value.z;
+			}
+		}
+	}
+} // IsoTools.PlayMaker.Actions
---
+++ Assets/IsoTools/PlayMaker/Actions/IsoSetVelocity.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using HutongGames.PlayMaker;
+using IsoTools.PlayMaker.Internal;
+
+namespace IsoTools.PlayMaker.Actions {
+	[ActionCategory("IsoTools")]
+	[HutongGames.PlayMaker.Tooltip(
+		"Sets the Velocity of a IsoRigidbody. " +
+		"To leave any axis unchanged, set variable to 'None'.")]
+	public class IsoSetVelocity : IsoComponentAction<IsoRigidbody> {
+		[RequiredField]
+		[CheckForComponent(typeof(IsoRigidbody))]
+		public FsmOwnerDefault gameObject;
+
+		[UIHint(UIHint.Variable)]
+		public FsmVector3 vector;
+
+		public FsmFloat x;
+		public FsmFloat y;
+		public FsmFloat z;
+
+		public bool everyFrame;
+
+		public override void Reset() {
+			gameObject  = null;
+			vector      = null;
+			x           = new FsmFloat{UseVariable = true};
+			y           = new FsmFloat{UseVariable = true};
+			z           = new FsmFloat{UseVariable = true};
+			everyFrame  = false;
+		}
+
+		public override void OnPreprocess() {
+			Fsm.HandleFixedUpdate = true;
+		}
+
+		public override void OnEnter() {
+			DoAction();
+			if ( !everyFrame ) {
+				Finish();
+			}
+		}
+
+		public override void OnFixedUpdate() {
+			DoAction();
+		}
+
+		void DoAction() {
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if ( UpdateCache(go) ) {
+				var value = vector.IsNone ? isoRigidbody.velocity : vector.Value;
+				if ( !x.IsNone ) { value.x = x.Value; }
+				if ( !y.IsNone ) { value.y = y.Value; }
+				if ( !z.IsNone ) { value.z = z.Value; }
+				isoRigidbody.velocity = value;
+			}
+		}
+	}
+} // IsoTools.PlayMaker.Actions
