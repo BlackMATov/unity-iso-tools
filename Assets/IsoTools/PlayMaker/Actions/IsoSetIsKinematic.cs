6ef010b147a7210fcae50df0cc6844efde877a1c
BlackMATov <blackmatov@gmail.com>
2015-12-20 21:04:59 +0600

    New PlayMaker actions: IsoIsUseGravity, IsoSetIsKinematic, IsoSetUseGravity, IsoWakeUp

---
+++ Assets/IsoTools/PlayMaker/Actions/IsoSetIsKinematic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using HutongGames.PlayMaker;
+using IsoTools.PlayMaker.Internal;
+
+namespace IsoTools.PlayMaker.Actions {
+	[ActionCategory("IsoTools.Physics")]
+	[HutongGames.PlayMaker.Tooltip(
+		"Sets the IsKinematic of a IsoRigidbody.")]
+	public class IsoSetIsKinematic : IsoComponentAction<IsoRigidbody> {
+		[RequiredField]
+		[CheckForComponent(typeof(IsoRigidbody))]
+		public FsmOwnerDefault gameObject;
+
+		[RequiredField]
+		public FsmBool isKinematic;
+
+		public override void Reset() {
+			gameObject  = null;
+			isKinematic = true;
+		}
+
+		public override void OnEnter() {
+			DoAction();
+			Finish();
+		}
+
+		void DoAction() {
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if ( UpdateCache(go) ) {
+				isoRigidbody.isKinematic = isKinematic.Value;
+			}
+		}
+	}
+} // IsoTools.PlayMaker.Actions
