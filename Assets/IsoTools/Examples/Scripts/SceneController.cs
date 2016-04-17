8d5a177eb1f8d9dde0281f0871959eb7a4546194
BlackMATov <blackmatov@gmail.com>
2016-04-17 18:20:25 +0600

    Canvas for examples with next and prev scene button

---
+++ Assets/IsoTools/Examples/Scripts/SceneController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_5_3_OR_NEWER
+using UnityEngine.SceneManagement;
+#endif
+
+namespace IsoTools.Examples {
+	public class SceneController : MonoBehaviour {
+		public Button NextSceneBtn  = null;
+		public Button PrevSceneBtn  = null;
+
+		public string NextSceneName = string.Empty;
+		public string PrevSceneName = string.Empty;
+
+		public void OnValidate() {
+			if ( !NextSceneBtn || !PrevSceneBtn ) {
+				Debug.LogError("SceneController. Wrong description!");
+			}
+		}
+
+		public void Start() {
+			if ( NextSceneBtn ) {
+				NextSceneBtn.interactable = !string.IsNullOrEmpty(NextSceneName);
+			}
+			if ( PrevSceneBtn ) {
+				PrevSceneBtn.interactable = !string.IsNullOrEmpty(PrevSceneName);
+			}
+		}
+
+		public void NextScene() {
+		#if UNITY_5_3_OR_NEWER
+			SceneManager.LoadScene(NextSceneName);
+		#else
+			Application.LoadLevel(NextSceneName);
+		#endif
+		}
+
+		public void PrevScene() {
+		#if UNITY_5_3_OR_NEWER
+			SceneManager.LoadScene(PrevSceneName);
+		#else
+			Application.LoadLevel(PrevSceneName);
+		#endif
+		}
+	}
+}
