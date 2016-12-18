985274a7109a9201b919d6683767fa93f49a0e1b
BlackMATov <blackmatov@gmail.com>
2016-12-19 03:01:47 +0700

    fix scenes to new isoworld child/parent system

--- Assets/IsoTools/Examples/Scripts/Kenney/AlienBallSpawner.cs
+++ Assets/IsoTools/Examples/Scripts/Kenney/AlienBallSpawner.cs
@@ -11,16 +11,17 @@ namespace IsoTools.Examples.Kenney {
 			if ( !alienBallPrefab ) {
 				throw new UnityException("AlienBallSpawner. Alien ball prefab not found!");
 			}
-			StartCoroutine(SpawnAlienBall());
+			var iso_world = IsoWorld.GetWorld(0);
+			StartCoroutine(SpawnAlienBall(iso_world));
 		}
 		
-		IEnumerator SpawnAlienBall() {
+		IEnumerator SpawnAlienBall(IsoWorld iso_world) {
 			while ( true ) {
 				var aliens = GameObject.FindObjectsOfType<AlienBallController>();
 				if ( aliens.Length < maxAlienCount ) {
 					var dx = Random.Range(2.0f, 3.0f);
 					var dy = Random.Range(2.0f, 3.0f);
-					var alien_ball_go = Instantiate(alienBallPrefab);
+					var alien_ball_go = Instantiate(alienBallPrefab, iso_world.transform);
 					var alien_iso_obj = alien_ball_go.GetComponent<IsoObject>();
 					alien_iso_obj.position = new Vector3(dx, dy, 5.0f);
 				}
